34099ee030107ea2bb52628f99a6be8191c427fc
Ekaterina Akinshina <akinshinaev86@gmail.com>
2022-08-06 22:29:17 +0300

    Final version Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -18,9 +18,13 @@ if(number2 > number1)
 {
     max = number2;
 }
-else if(number3 > number1)
+if(number3 > number1)
 {
     max = number3;
+}
+else if(number2 > number3)
+{
+    max = number2;
 }
 
 Console.WriteLine($"max = {max}");

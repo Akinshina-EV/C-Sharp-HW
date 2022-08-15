a8331aa38c1991b40589a3272ab8a4f3006e3949
Ekaterina Akinshina <akinshinaev86@gmail.com>
2022-08-15 09:16:17 +0300

    Comleted tasks of seminar 3

---
+++ Seminar3_HW/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if(number == new string (number.Reverse().ToArray()))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
---
+++ Seminar3_HW/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+
+class Program
+{
+    static double[] GetCoordinates(string pointName)
+    {
+    Console.WriteLine("Введите координаты точки " + pointName + " через запятую, например: 1,2,3: ");
+    return Console.ReadLine().Split(',').Select(double.Parse).ToArray();        
+    }
+    
+    static double DiffSqrd(double coordinateA, double coordinateB)
+    {
+        double diffSqrd = Math.Pow(coordinateB - coordinateA, 2);
+        return diffSqrd;
+    }
+
+    static void Main()
+    {
+    double[] pointA = GetCoordinates("A");
+    double[] pointB = GetCoordinates("B");
+
+    double distanceAB = Math.Sqrt(DiffSqrd(pointA[0], pointB[0]) + DiffSqrd(pointA[1], pointB[1]) + DiffSqrd(pointA[2], pointB[2]));
+    Console.WriteLine(Math.Round(distanceAB, 2));
+    }
+}

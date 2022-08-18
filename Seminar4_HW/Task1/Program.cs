afd6d812c2aa67c6358a6f35e6289b3548baa242
Ekaterina Akinshina <akinshinaev86@gmail.com>
2022-08-18 14:58:13 +0300

    Completed Tasks of Seminar 4

---
+++ Seminar4_HW/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+static int GetNumber(string numberName)
+{
+Console.WriteLine("Введите число " + numberName + ": ");
+return Convert.ToInt32(Console.ReadLine());     
+}
+
+int numberA = GetNumber("A");
+int numberB = GetNumber("B");
+
+int power = 1;
+
+for(int i = 0; i < numberB; i++)
+{
+    power *= numberA;
+}
+Console.WriteLine(power);
---
+++ Seminar4_HW/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//Данную задачу, как говорит нам Google, можно решить в три строки:
+// Console.WriteLine("Введите числа через запятую, которые требуется записать в массив: ");
+// int[] array = Console.ReadLine().Split(',').Select(Int32.Parse).ToArray();  
+// Console.WriteLine("[{0}]", string.Join(", ", array));
+
+//Но, если надо использовать цикл, то программа будет выглядить вот так:
+
+Console.WriteLine("Введите числа через запятую, которые требуется записать в массив: ");
+int[] array = Console.ReadLine().Split(',').Select(Int32.Parse).ToArray();  
+Console.Write("[");  
+for(int i = 0; i < array.Length-1; i++)
+{
+Console.Write(array[i] + ", ");  
+}
+Console.WriteLine(array[array.Length-1] + "]");
